18b436bd915ef075852e7b3e618e73fc7ed5987a
phulerasanju20 <saurav.phulera@ziploan.com>
2024-08-06 18:16:12 +0530

    landing page structured

---
+++ src/components/CommandPanel/CommandPanel.js
@@ -0,0 +1,43 @@
+import './CommandPanel.css';
+
+function CommandPanel() { 
+
+    return (
+        <div className='terminal-container' scoped>
+            <header className='terminal-header mt'>
+                <div className='flex terminal-header-buttons'>
+                    <div className='terminal-button close flex align-center justify-center'></div>
+                    <div className='terminal-button minimise'></div>
+                    <div className='terminal-button expand'></div>
+                </div>
+            </header>
+            <div className='terminal-body'>
+                <div>
+                    <p className='has-text-weight-bold'>
+                        Hello! I'm Saurav 
+                    </p>
+                    <br></br>
+                    <p>Here's some facts about me:</p>
+                    <ul>
+                        <li className='is-rainbow-red'>✅ HTML and CSS Developer</li>
+                        <li className='is-rainbow-orange'>✅ React Developer</li>
+                        <li className='is-rainbow-yellow'>✅ TypeScript Expert</li>
+                        <li className='is-rainbow-green'>✅ Angular Developer</li>
+                        <li className='is-rainbow-blue'>✅ Vue Developer</li>
+                        <li className='is-rainbow-violet'>✅ Chess Fan</li>
+                    </ul>
+
+                </div>
+                <div className='terminal-control mt-2'>
+                    <div className='control has-icons-left'>
+                        <span className='icon is-left'>{'>'}</span>
+                        <input className='input' type='text' autoComplete='off' autoFocus placeholder='Type commands' />
+                        <span className='cursor'></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CommandPanel;
---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,43 @@
+import CommandPanel from '../CommandPanel/CommandPanel';
+import './Landing.css';
+
+function Landing() {
+  return (
+    <div className="landing flex justify-center align-center">
+        <div className='container flex'>
+
+          {/* phone container */}
+            <div>
+              <div className='phone'>
+                  <div className='inner-screen flex align-center flex-col'>
+                    <div className='speaker'></div>
+                    <div className='phone-content text-center'>
+                        <h1 className='title title-font'>Hello! I am a Frontend Developer</h1>
+                        <div className='mt-2'>
+                          <img className='programmer-icon' src='/programmer.png' alt='programmer logo' />
+                        </div>
+                        <h2 className='subtitle title-font mt-2'>
+                          Want to build Something?
+                        </h2>
+                        <button className='mt-2 primary-button'>
+                          Lets Connect
+                        </button>
+                    </div>
+                  </div>
+              </div>
+            </div>
+
+            {/* command side */}
+            <div className='p'>
+              <div className='flex align-center'>
+                <div className='circle text-safe-glow blink mr'></div>
+                <div className='text-white recruitment-notifier'>Still Looking</div>
+              </div>
+                <CommandPanel />
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Landing;
